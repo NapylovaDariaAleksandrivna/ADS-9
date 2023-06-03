2687613d3e7b10ddd049d9ef1f8691762e7dd18f
DinoFF <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-06-03 14:19:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,27 @@
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> bst;
+    std::ifstream file(filename);
+    std::string output;
+    while (!file.eof()) {
+        char str = file.get();
+        if (str >= 65 && str <= 90) {
+            str += 32;
+            output += str;
+            continue;
+        }
+        if (str >= 97 && str <= 122) {
+            output += str;
+        } else {
+            if (!output.empty()) {
+                bst.add(output);
+            }
+            output.clear();
+        }
+    }
+    bst.add(output);
+    file.close();
+    return bst;
 }
